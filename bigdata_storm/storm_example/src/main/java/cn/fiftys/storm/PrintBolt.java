0ac2c7b9ca404a39fa82c0b88effef1487ec771a
yummy <873387751@qq.com>
2018-08-11 23:16:42 +0800

    storm课程应用案例:计算单词出现的次数Topology

---
+++ bigdata_storm/storm_example/src/main/java/cn/fiftys/storm/PrintBolt.java
@@ -0,0 +1,28 @@
+package cn.fiftys.storm;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.Map;
+
+public class PrintBolt extends BaseRichBolt {
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+
+    }
+
+    public void execute(Tuple input) {
+        String word = input.getStringByField("word");
+        Integer count = input.getIntegerByField("count");
+
+        //打印上流传递的数据
+        System.out.println(word + "----->" + count);
+
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
+}
